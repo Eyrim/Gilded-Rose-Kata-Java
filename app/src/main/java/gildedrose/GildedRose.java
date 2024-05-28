2b30f669e07376b35de5b102922d4e676bdbbdc7
Eyrim <marfxedbot@gmail.com>
2024-05-28 14:15:32 +0100

    Formatting, and explain reasoning for disputing test data

--- app/src/main/java/gildedrose/GildedRose.java
+++ app/src/main/java/gildedrose/GildedRose.java
@@ -1,18 +1,24 @@
 package gildedrose;
 
-import gildedrose.handlers.*;
-
 import java.util.Hashtable;
 import java.util.List;
+
+import gildedrose.handlers.AgedBrieHandler;
+import gildedrose.handlers.BackstagePassHandler;
+import gildedrose.handlers.FabergeEggHandler;
+import gildedrose.handlers.GenericItemHandler;
+import gildedrose.handlers.Handler;
 
 class GildedRose {
     List<Item> items;
 
-    private final Hashtable<String, Handler> ITEM_NAME_HANDLER_MAP = new Hashtable<>() {{
-        put("Aged Brie", new AgedBrieHandler());
-        put("Backstage passes to a Coldplay concert", new BackstagePassHandler());
-        put("Fabergé egg", new FabergeEggHandler());
-    }};
+    private final Hashtable<String, Handler> ITEM_NAME_HANDLER_MAP = new Hashtable<>() {
+        {
+            put("Aged Brie", new AgedBrieHandler());
+            put("Backstage passes to a Coldplay concert", new BackstagePassHandler());
+            put("Fabergé egg", new FabergeEggHandler());
+        }
+    };
 
     private final GenericItemHandler GENERIC_ITEM_HANDLER = new GenericItemHandler();
 
@@ -30,72 +36,73 @@ class GildedRose {
 
     public void updateQuality() {
         items = items.stream()
-            .map(this::findHandler)
-            .toList();
+                .map(this::findHandler)
+                .toList();
 
-//        for (int i = 0; i < items.size(); i++) {
-//            if (!items.get(i).name.equals("Aged Brie")
-//                    && !items.get(i).name.equals("Backstage passes to a Coldplay concert")) {
-//                if (items.get(i).quality > 0) {
-//                    if (!items.get(i).name.equals("Fabergé egg")) {
-//                        items.get(i).quality = items.get(i).quality - 1;
-//                    }
-//                }
-//            } else {
-//                if (items.get(i).quality < 50) {
-//                    items.get(i).quality = items.get(i).quality + 1;
-//
-//                    if (items.get(i).name.equals("Backstage passes to a Coldplay concert")) {
-//                        if (items.get(i).sellIn < 11) {
-//                            if (items.get(i).quality < 50) {
-//                                items.get(i).quality = items.get(i).quality + 1;
-//                            }
-//                        }
-//
-//                        if (items.get(i).sellIn < 6) {
-//                            if (items.get(i).quality < 50) {
-//                                items.get(i).quality = items.get(i).quality + 1;
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//
-//            if (!items.get(i).name.equals("Fabergé egg")) {
-//                items.get(i).sellIn = items.get(i).sellIn - 1;
-//            }
-//
-//            if (items.get(i).sellIn < 0) {
-//                if (!items.get(i).name.equals("Aged Brie")) {
-//                    if (!items.get(i).name.equals("Backstage passes to a Coldplay concert")) {
-//                        if (items.get(i).quality > 0) {
-//                            if (!items.get(i).name.equals("Fabergé egg")) {
-//                                items.get(i).quality = items.get(i).quality - 1;
-//                            }
-//                        }
-//                    } else {
-//                        items.get(i).quality = items.get(i).quality - items.get(i).quality;
-//                    }
-//                } else {
-//                    if (items.get(i).quality < 50) {
-//                        items.get(i).quality = items.get(i).quality + 1;
-//                    }
-//                }
-//            }
-//        }
+        // for (int i = 0; i < items.size(); i++) {
+        // if (!items.get(i).name.equals("Aged Brie")
+        // && !items.get(i).name.equals("Backstage passes to a Coldplay concert")) {
+        // if (items.get(i).quality > 0) {
+        // if (!items.get(i).name.equals("Fabergé egg")) {
+        // items.get(i).quality = items.get(i).quality - 1;
+        // }
+        // }
+        // } else {
+        // if (items.get(i).quality < 50) {
+        // items.get(i).quality = items.get(i).quality + 1;
+        //
+        // if (items.get(i).name.equals("Backstage passes to a Coldplay concert")) {
+        // if (items.get(i).sellIn < 11) {
+        // if (items.get(i).quality < 50) {
+        // items.get(i).quality = items.get(i).quality + 1;
+        // }
+        // }
+        //
+        // if (items.get(i).sellIn < 6) {
+        // if (items.get(i).quality < 50) {
+        // items.get(i).quality = items.get(i).quality + 1;
+        // }
+        // }
+        // }
+        // }
+        // }
+        //
+        // if (!items.get(i).name.equals("Fabergé egg")) {
+        // items.get(i).sellIn = items.get(i).sellIn - 1;
+        // }
+        //
+        // if (items.get(i).sellIn < 0) {
+        // if (!items.get(i).name.equals("Aged Brie")) {
+        // if (!items.get(i).name.equals("Backstage passes to a Coldplay concert")) {
+        // if (items.get(i).quality > 0) {
+        // if (!items.get(i).name.equals("Fabergé egg")) {
+        // items.get(i).quality = items.get(i).quality - 1;
+        // }
+        // }
+        // } else {
+        // items.get(i).quality = items.get(i).quality - items.get(i).quality;
+        // }
+        // } else {
+        // if (items.get(i).quality < 50) {
+        // items.get(i).quality = items.get(i).quality + 1;
+        // }
+        // }
+        // }
+        // }
     }
-    
-   @Override
-   public String toString() {
-        var output = "";
-        for (var i = 0; i < items.size(); i++)
-        {
-            if (output.length() > 0)
-            {
+
+    @Override
+    public String toString() {
+        String output = "";
+
+        for (var i = 0; i < items.size(); i++) {
+            if (output.length() > 0) {
                 output += ", ";
             }
+
             output += items.get(i).toString();
         }
+
         return output;
     }
 }
